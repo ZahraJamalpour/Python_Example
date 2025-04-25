aba34bdcb52ee9788d21e0aa96c4f6c182b3a7c9
Zahra <zahrajamalpoor74@gmail.com>
2025-04-25 11:37:58 +0330

    added Ex04 and Ex05

---
+++ Ex04_Ex05/CONDITION_LOOP.py
@@ -0,0 +1,11 @@
+# exercise 03: condition
+# queston 1
+# string=color_product
+color_product=0
+for i in range(2):
+    color_product =input("color_product: ")
+    if i == 2:
+        continue
+    print(i)
+else:
+    print(i==2, i==1, i==2)
